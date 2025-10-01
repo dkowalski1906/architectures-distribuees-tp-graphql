a07cbb2a50d0d8734117a6ca52593f95fc714ecf
Damien Kowalski <damien.kowalski@gmail.com>
2025-10-01 23:34:47 +0200

    add movie.graphql and resolvers.py

--- movie/movie.py
+++ movie/movie.py
@@ -1,7 +1,9 @@
+from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
 from flask import Flask, request, jsonify, make_response
 import time, json, requests
 from werkzeug.exceptions import NotFound
 from flask_cors import CORS
+import resolvers as r
 
 app = Flask(__name__)
 
@@ -16,6 +18,24 @@ CACHE_TTL = 60 # secondes de validité du cache pour is_admin
 # cache local pour stocker si un user est admin
 # format : { "user_id": {"is_admin": True/False, "timestamp": 123456789} }
 user_admin_cache = {}
+
+# Création du schéma GraphQL
+type_defs = load_schema_from_path('movie.graphql')
+
+query = QueryType()
+mutation = MutationType()
+
+movie = ObjectType('Movie')
+
+query.set_field('movie_with_id', r.movie_with_id)
+query.set_field('movie_with_title', r.movie_with_title)
+query.set_field('movies_json', r.movies_json)
+
+mutation.set_field('add_movie', r.add_movie)
+mutation.set_field('update_movie_rate', r.update_movie_rate)
+mutation.set_field('remove_movie_with_id', r.remove_movie_with_id)
+
+schema = make_executable_schema(type_defs, movie, query, mutation)
 
 # charge le fichier JSON contenant les films
 with open('{}/databases/movies.json'.format("."), 'r') as jsf:
@@ -75,180 +95,18 @@ def home():
     """
     return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
 
-# retourne tous les films en JSON brut
-@app.route("/<user_id>/movies/json", methods=['GET'])
-def get_json(user_id):
-    """
-    Get all movies in raw JSON format.
-
-    Args:
-        user_id (str): ID of the requesting user.
-
-    Returns:
-        Response: JSON response with the list of all movies,
-                  or an error if user is not authorized.
-    """
-    _, error = verify_admin(user_id)
-    if error:
-        return error
-
-    res = make_response(jsonify(movies), 200)
-    return res
-
-# retourne un film à partir de son ID
-@app.route("/<user_id>/movies/<movie_id>", methods=['GET'])
-def get_movie_by_id(user_id, movie_id):
-    """
-    Get a movie by its ID.
-
-    Args:
-        user_id (str): ID of the requesting user.
-        movie_id (str): ID of the movie to retrieve.
-
-    Returns:
-        Response: JSON response with movie details if found,
-                  or an error if movie ID is not found.
-    """
-    _, error = verify_admin(user_id)
-    if error:
-        return error
-
-    for movie in movies:
-        if str(movie["id"]) == str(movie_id):
-            res = make_response(jsonify(movie),200)
-            return res
-    return make_response(jsonify({"error":"Movie ID not found"}),500)
-
-# retourne un film à partir de son titre
-@app.route("/<user_id>/movies/by_title", methods=['GET'])
-def get_movie_by_title(user_id):
-    """
-    Get a movie by its title.
-
-    Args:
-        user_id (str): ID of the requesting user.
-
-    Query Parameters:
-        title (str): Title of the movie to search for.
-
-    Returns:
-        Response: JSON response with movie details if found,
-                  or an error if title is not found.
-    """
-    _, error = verify_admin(user_id)
-    if error:
-        return error
-    
-    json = ""
-    if request.args:
-        req = request.args
-        for movie in movies:
-            if str(movie["title"]) == str(req["title"]):
-                json = movie
-
-    if not json:
-        res = make_response(jsonify({"error":"movie title not found"}),500)
-    else:
-        res = make_response(jsonify(json),200)
-    return res
-
-# ajoute un nouveau film
-@app.route("/<user_id>/movies/<movie_id>", methods=['POST'])
-def add_movie(user_id, movie_id):
-    """
-    Add a new movie.
-
-    Args:
-        user_id (str): ID of the requesting user.
-        movie_id (str): ID for the new movie.
-
-    Request Body:
-        JSON object with movie details (id, title, rating, etc.)
-
-    Returns:
-        Response: JSON message confirming addition,
-                  or error if movie ID already exists or user is not admin.
-    """
-    is_admin, error = verify_admin(user_id)
-    if error:
-        return error
-
-    # si pas admin -> accès interdit
-    if not is_admin:
-        return make_response(jsonify({"error": "Unauthorized: admin access required"}), 403)
-
-    req = request.get_json()
-
-    for movie in movies:
-        if str(movie["id"]) == str(movie_id):
-            print(movie["id"])
-            print(movie_id)
-            return make_response(jsonify({"error":"movie ID already exists"}),500)
-
-    movies.append(req)
-    write(movies)
-    res = make_response(jsonify({"message":"movie added"}),200)
-    return res
-
-#modifie le score d'un film existant
-@app.route("/<user_id>/movies/<movie_id>/<rate>", methods=['PUT'])
-def update_movie_rating(user_id, movie_id, rate):
-    """
-    Update the rating of an existing movie.
-
-    Args:
-        user_id (str): ID of the requesting user.
-        movie_id (str): ID of the movie to update.
-        rate (str or float): New rating for the movie.
-
-    Returns:
-        Response: JSON response with updated movie data,
-                  or error if movie ID is not found.
-    """
-    _, error = verify_admin(user_id)
-    if error:
-        return error
-
-    for movie in movies:
-        if str(movie["id"]) == str(movie_id):
-            movie["rating"] = rate
-            res = make_response(jsonify(movie),200)
-            write(movies)
-            return res
-
-    res = make_response(jsonify({"error":"movie ID not found"}),500)
-    return res
-
-# supprime un film à partir de son ID
-@app.route("/<user_id>/movies/<movie_id>", methods=['DELETE'])
-def delete_movie(user_id, movie_id):
-    """
-    Delete a movie by its ID.
-
-    Args:
-        user_id (str): ID of the requesting user.
-        movie_id (str): ID of the movie to delete.
-
-    Returns:
-        Response: JSON response confirming deletion,
-                  or error if movie ID is not found or user is not admin.
-    """
-    is_admin, error = verify_admin(user_id)
-    if error:
-        return error
-
-    # si pas admin -> accès interdit
-    if not is_admin:
-        return make_response(jsonify({"error": "Unauthorized: admin access required"}), 403)
-
-    for movie in movies:
-        if str(movie["id"]) == str(movie_id):
-            movies.remove(movie)
-            write(movies)
-            return make_response(jsonify(movie),200)
-
-    res = make_response(jsonify({"error":"movie ID not found"}),500)
-    return res
+# route GraphQL
+@app.route('/graphql', methods=['POST'])
+def graphql_server():
+    data = request.get_json()
+    success, result = graphql_sync(
+                        schema,
+                        data,
+                        context_value=None,
+                        debug=app.debug
+                    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
 
 if __name__ == "__main__":
     #p = sys.argv[1]
